8716e5d77993d8554c9f24cbb9d0640e80f06abf
zGuschin <93988913+zGuschin@users.noreply.github.com>
2023-02-13 17:54:55 +0300

    Update icons22.php

--- mods/icons22.php
+++ mods/icons22.php
@@ -1,3 +1,3 @@
-<?php
-header('Location: https://github.com/zGuschin/ICONKITGD-2.2-LEAKED-MOD/releases/download/ver-1.0.22/RuhaxIcons22-ver-1.0.22.zip ');
-?>
+<script>
+window.location.replace('https://github.com/zGuschin/ICONKITGD-2.2-LEAKED-MOD/releases/download/ver-1.0.22/RuhaxIcons22-ver-1.0.22.zip')
+</script>
